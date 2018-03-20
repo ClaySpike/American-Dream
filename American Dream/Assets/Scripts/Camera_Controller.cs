b97201183acdef25da6b26bcf1ee4bfc19c170a4
ClaySpike <povinellimax@gmail.com>
2018-03-20 19:18:07 -0400

    Camera and Player Control
    Camera and Player Control
    Tried to make joycons work but now inputs for controllers are messed up
    need to fix red lines on some of the tiles

---
+++ American Dream/Assets/Scripts/Camera_Controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour {
+
+    private static bool cameraExists;
+
+    public GameObject target;
+    private Vector3 targetPos;
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+        if (!cameraExists)
+        {
+            cameraExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void FixedUpdate()
+    {
+        targetPos = new Vector3(target.transform.position.x, target.transform.position.y, -10f);
+        transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed);
+    }
+}
