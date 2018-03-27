df029861cdcdea7c17209225058b51ec37fc22be
ClaySpike <33270607+ClaySpike@users.noreply.github.com>
2018-03-26 21:14:15 -0400

    Created Scripts to load new scenes
    -Changed Player_Movement to Player_Controller
    -Created Load_New_Area script
    -Created Player_Start_Point script
    -Did lotso thinking

---
+++ American Dream/Assets/Scripts/Player_Start_Point.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Start_Point : MonoBehaviour {
+
+    public Player_Controller thePlayer;
+    public Camera_Controller theCamera;
+
+    public Vector2 startDirection;
+
+	// Use this for initialization
+	void Start () {
+        thePlayer = FindObjectOfType<Player_Controller>();
+        theCamera = FindObjectOfType<Camera_Controller>();
+
+        thePlayer.transform.position = transform.position;
+
+        theCamera.transform.position = new Vector3(transform.position.x, transform.position.y, theCamera.transform.position.z);
+
+        thePlayer.lastMove = startDirection;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
